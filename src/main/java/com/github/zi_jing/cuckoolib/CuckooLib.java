7c44a1dfc4ee50b748eb0f6ea7f41c637182b1a3
zi-jing <3056894830@qq.com>
2020-06-07 19:03:35 +0800

    在build.gradle中添加归档json生成代码

---
+++ src/main/java/com/github/zi_jing/cuckoolib/CuckooLib.java
@@ -0,0 +1,34 @@
+package com.github.zi_jing.cuckoolib;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = CuckooLib.MODID, name = CuckooLib.NAME, version = CuckooLib.VERSION, useMetadata = true)
+public class CuckooLib {
+    public static final String MODID = "cuckoolib";
+    public static final String NAME = "CuckooLib";
+    public static final String VERSION = "@CUCKOO_LIB_VERSION@";
+
+    @Mod.Instance
+    public static CuckooLib instance;
+
+    private static Logger logger;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    public static Logger getLogger() {
+        return logger;
+    }
+}
