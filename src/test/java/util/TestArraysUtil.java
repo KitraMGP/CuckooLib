0b56bceb4b54f2cabdea04cbc3cbf4c0856c06ae
zi-jing <3056894830@qq.com>
2020-07-21 23:32:05 +0800

    添加单元测试，DocumentRenderer重构完成50%

---
+++ src/test/java/util/TestArraysUtil.java
@@ -0,0 +1,16 @@
+package util;
+
+import com.github.zi_jing.cuckoolib.util.ArraysUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestArraysUtil {
+    @Test
+    public void testConcat() {
+        String[] input1 = {"abc", "def", "ghi"};
+        String[] input2 = {"str1", "str2", "str3"};
+        String[] input3 = {"str4"};
+        String[] answer = {"abc", "def", "ghi", "str1", "str2", "str3", "str4"};
+        Assertions.assertArrayEquals(answer, ArraysUtil.concat(input1, input2, input3));
+    }
+}
