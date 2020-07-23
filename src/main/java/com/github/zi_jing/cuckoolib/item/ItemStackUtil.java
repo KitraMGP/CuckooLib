2c0739839b456d01c43e6b6b194f0cc6ac11f073
zi-jing <3056894830@qq.com>
2020-07-23 12:21:27 +0800

    整理包结构

---
+++ src/main/java/com/github/zi_jing/cuckoolib/item/ItemStackUtil.java
@@ -0,0 +1,24 @@
+package com.github.zi_jing.cuckoolib.item;
+
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class ItemStackUtil {
+    /**
+     * 使用原版方式在世界中生成一个掉落物实体
+     *
+     * @param world 生成位置所在的世界
+     * @param pos   生成位置
+     * @param stack 要生成的{@link ItemStack}
+     */
+    public static void dropItem(World world, BlockPos pos, ItemStack stack) {
+        double offX = (double) (world.rand.nextFloat() * 0.5F) + 0.25D;
+        double offY = (double) (world.rand.nextFloat() * 0.5F) + 0.25D;
+        double offZ = (double) (world.rand.nextFloat() * 0.5F) + 0.25D;
+        EntityItem entityitem = new EntityItem(world, (double) pos.getX() + offX, (double) pos.getY() + offY, (double) pos.getZ() + offZ, stack);
+        entityitem.setDefaultPickupDelay();
+        world.spawnEntity(entityitem);
+    }
+}
